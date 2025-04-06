aaa1b3fabdc177424ea742485e5dd76b71a78a37
iukmicro <iukmicrocom@gmail.com>
2025-04-06 15:20:28 +0530

    nextjs updates

--- neo-vision/next.config.mjs
+++ neo-vision/next.config.mjs
@@ -27,6 +27,9 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
+  output: 'export',
+  basePath: '/NEO_VISION',
+  assetPrefix: '/NEO_VISION/',
 }
 
 if (userConfig) {
